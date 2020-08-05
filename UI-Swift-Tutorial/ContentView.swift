740beefb3e4ad71f253342da6c99692f11831bd3
habib <durodolahabib043@gmail.com>
2020-08-04 20:25:55 -0400

    first commit

---
+++ UI-Swift-Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI-Swift-Tutorial
+//
+//  Created by Habib Durodola on 2020-08-04.
+//  Copyright © 2020 DOH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI-Swift-TutorialTests/UI_Swift_TutorialTests.swift
@@ -0,0 +1,34 @@
+//
+//  UI_Swift_TutorialTests.swift
+//  UI-Swift-TutorialTests
+//
+//  Created by Habib Durodola on 2020-08-04.
+//  Copyright © 2020 DOH. All rights reserved.
+//
+
+import XCTest
+@testable import UI_Swift_Tutorial
+
+class UI_Swift_TutorialTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
